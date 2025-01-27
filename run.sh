481d8c67358318f822812a613945032fc652d178
Mattia Braga <mattiabraga2006@gmail.com>
2025-01-27 23:10:00 +0100

    run

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd /site/LOL_analiz
+
+
+setup_venv() {
+    echo "Setting up virtual environment..."
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip3 install -r requirements.txt
+}
+
+if [ ! -d ".venv" ]; then
+    setup_venv
+else
+    source .venv/bin/activate
+fi
+
+cd bin
+
+echo "Starting Python API..."
+uvicorn main:app --host 0.0.0.0 --port 8006 &
+
+wait
